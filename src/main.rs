d8641b333097161d6c92b443a7907e0c5c74f2f5
BxsedSosa <donovanxabad@gmail.com>
2026-03-05 07:12:57 -0800

    getting started with text prompts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+fn clear_console() {
+    print!("\x1B[2J")
+}
+
+fn sleep(secs: u64) {
+    std::thread::sleep(std::time::Duration::from_secs(secs));
+}
+
+fn welcome() {
+    clear_console();
+    println!("Welcome to Todo List");
+    sleep(2);
+}
+
+fn menu_selection() {
+    clear_console();
+    println!("Make a selection:\n1) Add Todo\n2) Edit Todo\n3) Delete Todo\n4) Exit Program")
+}
+
 fn main() {
-    println!("Hello, world!");
+    welcome();
+    menu_selection();
 }
